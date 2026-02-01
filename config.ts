a328728d0a8a109f3671f42156b8eb5fb53814dc
maxtechv-star <maxtechv@gmail.com>
2026-02-01 09:04:38 +0300

    Update session

--- config.ts
+++ config.ts
@@ -26,7 +26,7 @@ const defaults: Record<string, any> = {
   ANTIDELETE_MODE: 'off',
   AUTOVIEW_STATUS: false,
   AUTOLIKE_STATUS: false,
-  MENU_THEME: 'random' //select available 1-5 or leave random for random 
+  MENU_THEME: 'random'
 }
 
 let cache: ConfigCache = {}
